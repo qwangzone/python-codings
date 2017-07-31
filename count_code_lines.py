4f3a876fab12a1b66d4d06ba069c117cffd2f170
qwangzone <578380132@qq.com>
2017-07-31 11:23:45 +0800

    优化

--- count_code_lines.py
+++ count_code_lines.py
@@ -3,32 +3,37 @@ import os
 class CountCodeLines:
     def __init__(self, directory):
         self.directory = directory
+        self.start = 1
+        self.line_null = 0
+
+    def list_all_file(self):
+        #file_list = []
+        dirs = os.listdir(self.directory)
+        #for file in dirs:
+        #    file_list.append(file)
+        #return file_list
+        file_list = [f for f in dirs if f.split(".")[-1] == 'py']
+        return file_list
 
     def count_code(self):
-        start = 1
-        line_null = 0
-        dirs = os.listdir(self.directory)
-        for file in dirs:
-            if file.split('.')[-1] == "txt":
-                print (file)
-                with open(file, 'r') as f:
-                    for l in f.readlines():
-                        print (str(start) + "、" + l)
-                        start = start + 1
-                        if l.strip() == "":
-                            line_null = line_null + 1
+        for file in self.list_all_file():
+            with open(file, 'rb') as f:
+                for l in f.readlines():
+                    print (str(self.start) + "、" + l.decode('utf8'))
+                    self.start = self.start + 1
+                    if l.decode('utf8').strip() == "":
+                        self.line_null = self.line_null + 1
+        return self
 
-        print (u"代码行数为：" + str(start-1))
-        print (u"其中空行为：" + str(line_null))
+    def display(self):
+        print (u"代码行数为：" + str(self.start-1))
+        print (u"其中空行为：" + str(self.line_null))
 
 
 
 
 
 if __name__ == '__main__':
-    file = sys.argv[1:][0]
-    print (file)
-    CountCodeLines(file).count_code()
-
-
-
+    directory = sys.argv[1:][0]
+    print (directory)
+    CountCodeLines(directory).count_code().display()
